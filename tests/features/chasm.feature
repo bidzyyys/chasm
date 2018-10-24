9f2ded70f7a80e02383340a87d5f6f86162530c5
Daniel Bigos <daniel.bigos96@gmail.com>
2018-10-24 20:50:12 +0200

    Init

---
+++ tests/features/chasm.feature
@@ -0,0 +1,6 @@
+# Feature: chasm performance
+
+#  Scenario: run main program loop
+#     Given we have chasm installed
+#      when we run program
+#      then Something will be displayed
---
+++ tests/features/steps/chasm_steps.py
@@ -0,0 +1,13 @@
+# from behave import given, when, then
+
+#@given(u'we have chasm installed')
+#def step_impl(context):
+#    raise NotImplementedError(u'STEP: Given we have chasm installed')
+
+#@when(u'we run program')
+#def step_impl(context):
+#    raise NotImplementedError(u'STEP: When we run program')
+
+#@then(u'<species> will be displayed')
+#def step_impl(context):
+#    raise NotImplementedError(u'STEP: Then Something will be displayed')
