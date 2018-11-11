6e84cd6c108f99bc2a0642c61fa373152acbb514
Piotr Żelazko <piotr.zelazko@icloud.com>
2018-11-11 23:55:33 +0100

    feat: add Unlock Deposit transaction type
    Refers #8

--- src/chasm/primitives/transaction/UnlockDepositTransaction.h
+++ src/chasm/primitives/transaction/UnlockDepositTransaction.h
@@ -6,6 +6,7 @@
 #define CHASM_UNLOCK_DEPOSIT_TRANSACTION_H
 
 #include <chasm/primitives/Transaction.h>
+#include <vector>
 
 namespace chasm::primitives::transaction {
 
@@ -19,10 +20,16 @@ namespace chasm::primitives::transaction {
      *  This type of a transaction is also send in when an offer has not been accepted
      *  and the confirmation fee should unlocked.
      */
-    struct UnlockDepositTransaction : public Transaction {
+    class UnlockDepositTransaction : public Transaction {
 
+    private:
+        using proof_t = std::vector<std::byte>;
 
+        common::types::hash_t offerHash_;
+        common::types::token_t token_;
+        proof_t proof_;
 
+        common::types::out_idx_t bailIndex_;
     };
 }
 
