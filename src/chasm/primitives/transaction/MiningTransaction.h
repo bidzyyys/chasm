23dcdad3042aec1eb22b5afef9041399fe237f3e
Piotr Żelazko <piotr.zelazko@icloud.com>
2018-11-11 22:53:42 +0100

    doc: add Mining Transaction description
    Refers #8

--- src/chasm/primitives/transaction/MiningTransaction.h
+++ src/chasm/primitives/transaction/MiningTransaction.h
@@ -8,7 +8,17 @@
 #include <chasm/primitives/Transaction.h>
 
 namespace chasm::primitives::transaction{
+
+    /*!
+     * \brief Transaction of type mining.
+     *
+     * This transaction has no inputs and should produce \i XPC tokens.
+     * Starting from 50.0 per block and halving each 10^6 blocks.
+     * This is also the place, where fees from other transactions can be collected.
+     * In each block can exist only one transaction of this type and must be placed as the first one.
+     */
     class MiningTransaction : public Transaction {
+
     };
 }
 
