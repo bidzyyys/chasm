e87b027881162d0a6483b7ef4d2b0da1246f58c0
Piotr Żelazko <piotr.zelazko@icloud.com>
2018-11-12 16:30:58 +0100

    feat: add confirmation transaction
    Refers #7

--- src/chasm/primitives/transaction/ConfirmationTransaction.hpp
+++ src/chasm/primitives/transaction/ConfirmationTransaction.hpp
@@ -5,4 +5,33 @@
 #ifndef CHASM_CONFIRMATION_TRANSACTION_H
 #define CHASM_CONFIRMATION_TRANSACTION_H
 
+#include <chasm/primitives/Serializable.hpp>
+#include <chasm/common/types.hpp>
+
+namespace chasm::primitives::transaction {
+
+    /*!
+     * \brief Transaction of type confirmation
+     *
+     * This transaction can be send by anyone who knows the proofs of inclusion of both sides of the exchange on the
+     * corresponding chains.
+     * Inputs to this transaction can be of type FeeOutput, but only those referencing the exchange to be confirmed
+     */
+    class ConfirmationTransaction : public Serializable {
+    public:
+
+        ~ConfirmationTransaction() override = default;
+
+    private:
+
+        using proof_t = std::vector<std::byte>; //TODO: change
+
+        common::types::hash_t offerHash_;
+
+        proof_t proofTokenIn_;
+        proof_t proofTokenOut_;
+
+    };
+}
+
 #endif //CHASM_CONFIRMATION_TRANSACTION_H
