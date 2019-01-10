10ab1be38cd83ea5a42b056a2411cd6bf94e7bac
Piotr Żelazko <piotr.zelazko@icloud.com>
2019-01-10 02:06:14 +0100

    feat: start using configs in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import configparser
+import os
 from collections import namedtuple
 
 from ecdsa import SigningKey
@@ -11,6 +13,17 @@ Entity = namedtuple("Entity", "priv pub")
 def __entity__():
     key = SigningKey.generate(consensus.CURVE)
     return Entity(key.to_string(), key.get_verifying_key().to_string())
+
+
+@fixture(scope="session")
+def config():
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    config_dir = os.path.abspath(os.path.join(dir_path, "../config"))
+    config_files = [ os.path.join(config_dir, file) for file in ['config.ini', 'dev.ini']]
+
+    parser = configparser.ConfigParser()
+    parser.read(config_files)
+    return parser
 
 
 @fixture(scope="session")
