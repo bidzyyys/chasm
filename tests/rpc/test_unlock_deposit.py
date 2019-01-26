078c75b2b33619a9c3fc34e163e799de471635f6
Daniel Bigos <daniel.bigos96@gmail.com>
2019-01-26 19:28:30 +0100

    fix: fix unlocking deposit test

--- tests/rpc/test_unlock_deposit.py
+++ tests/rpc/test_unlock_deposit.py
@@ -4,7 +4,7 @@ from pytest_bdd import scenario, given, when, then, parsers
 from chasm.consensus import Side
 from chasm.consensus.primitives.transaction import UnlockingDepositTransaction
 from chasm.rpc import client
-from chasm.rpc.client import get_transaction, do_unlock_deposit
+from chasm.rpc.client import get_transaction, do_unlock_deposit, do_offer_match
 from . import init_address, mock_acceptance, sleep_for_block
 
 
@@ -13,33 +13,61 @@ def test_unlock_deposit():
     pass
 
 
-@given(parsers.parse('{owner} has {xpc:d} bdzys in {utxos:d} UTXO'))
-def parameters(chasm_server, alice_account, owner, xpc, utxos):
-    key, address = alice_account
-    init_address(address=address, balance=xpc, utxos=utxos)
+@given(
+    parsers.parse('{maker} has {xpc1:d} bdzys in {utxos1:d} UTXO and {taker} has {xpc2:d} bdzys in {utxos2:d} UTXO'))
+def parameters(chasm_server, alice_account, bob_account, maker, xpc1, utxos1, taker, xpc2, utxos2):
+    key1, addr1 = alice_account
+    init_address(addr1, balance=xpc1, utxos=utxos1)
+    key2, addr2 = bob_account
+    init_address(addr2, balance=xpc2, utxos=utxos2)
     return {
-        owner: {
-            "address": address,
-            "key": key
+        maker: {
+            "address": addr1,
+            "key": key1
+        },
+        taker: {
+            "address": addr2,
+            "key": key2
         },
         "offer": "",
-        "unlock": ""
+        "match": "",
+        "confirmation": ""
     }
 
 
 @when(parsers.parse(
-    '{maker} creates an exchange offer'))
+    '{maker} creates exchange offer: {amount:d} {token} for {price:d} {expected}'))
 def create_offer(parameters, node, test_port, publish_offer,
-                 maker):
+                 maker, amount, token, price, expected):
     sleep_for_block()
     client.input = mock_acceptance
     offer = publish_offer(parameters[maker]["address"],
                           parameters[maker]["key"],
-                          parameters[maker]["address"])
+                          parameters[maker]["address"],
+                          token, amount, expected, price)
 
     sleep_for_block()
+    parameters[maker]["receive"] = parameters[maker]["address"]
     parameters["offer"] = offer.hex()
-    assert get_transaction(node=node, port=test_port, transaction=parameters["offer"]) is not None
+    assert get_transaction(node=node, port=test_port,
+                           transaction=parameters["offer"]) is not None
+
+
+@when(parsers.parse(
+    '{taker} accepts the offer'))
+def accept_offer(parameters, node, test_port, btc_addr, datadir, taker):
+    client.input = mock_acceptance
+    parameters[taker]["receive"] = btc_addr.hex()
+    result, match = do_offer_match(node=node, port=test_port,
+                                   sender=parameters[taker]["address"],
+                                   offer_hash=parameters["offer"],
+                                   receive_addr=btc_addr.hex(), tx_fee=1,
+                                   conf_fee=2, deposit=12,
+                                   datadir=datadir,
+                                   signing_key=parameters[taker]["key"])
+    assert result
+    parameters["match"] = match.hash().hex()
+    sleep_for_block()
 
 
 @when(parsers.parse('{maker} creates UnlockDeposit transaction, deposit {deposit:d}, transaction fee {tx_fee:d}'))
